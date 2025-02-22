78c4dbe07d07fffaddefb46a52f43682bef10915
Deepesh Raman <v-draman@expediagroup.com>
2025-02-22 23:58:59 +0530

    contest sheet embedded

--- js/l10n.js
+++ js/l10n.js
@@ -1,1 +1,22 @@
+var title_txt = "SPSS ദേശീയ സമ്മേളനം"
+
+var email_txt = "yuvavedi.ekm@gmail.com"
+var phone_txt = "+91-00000 00000"
+
 var location_bus_txt = "തൃശൂർ, ആലപ്പുഴ ഭാഗങ്ങളിൽ നിന്ന് വരുന്നവർ വൈറ്റില ഹബ്ബില്‍ ഇറങ്ങി തൃപ്പൂണിത്തുറ ഭാഗത്തേക്ക് പോകുന്ന ബസ്സിൽ കയറി പേട്ട ബസ്സ് സ്റ്റോപ്പിൽ ഇറങ്ങുക. കോട്ടയം ഭാഗത്ത് നിന്ന് വരുന്നവർ എറണാകുളം ഭാഗത്തേക്ക് പോകുന്ന ബസ്സിൽ കയറി പേട്ട സ്റ്റോപ്പിൽ ഇറങ്ങുക.";
+var location_train_txt = "ആലപ്പുഴ ഭാഗത്തുനിന്ന് വരുന്നവർ എറണാകുളം ജംഗ്ഷൻ റെയിൽവേ സ്റ്റേഷനിൽ ഇറങ്ങി തൃപ്പൂണിത്തുറ ഭാഗത്തേക്ക് പോകുന്ന മെട്രോയിൽ കയറി പേട്ട മെട്രോ സ്റ്റേഷനിൽ ഇറങ്ങുക. തൃശ്ശൂർ ഭാഗത്തുനിന്നും വരുന്നവർ എറണാകുളം ജംഗ്ഷൻ സ്റ്റേഷനിലോ എറണാകുളം ടൗൺ സ്റ്റേഷനിലോ അല്ലെങ്കിൽ തൃപ്പൂണിത്തുറ സ്റ്റേഷനിലോ ഇറങ്ങി മെട്രോ പിടിച്ച് പേട്ട മെട്രോ സ്റ്റേഷനിൽ ഇറങ്ങുക. കോട്ടയം ഭാഗത്തുനിന്ന് വരുന്നവർ തൃപ്പൂണിത്തുറ റെയിൽവേ സ്റ്റേഷനിൽ ഇറങ്ങി മെട്രോ പിടിച്ച് പേട്ട മെട്രോ സ്റ്റേഷനിൽ ഇറങ്ങുക.";
+var location_metro_txt = "തൃശൂർ, ആലപ്പുഴ ഭാഗങ്ങളിൽ നിന്ന് വരുന്നവർ വൈറ്റില ഹബ്ബില്‍ ഇറങ്ങി തൃപ്പൂണിത്തുറ ഭാഗത്തേക്ക് പോകുന്ന മെട്രോയിൽ കയറി പേട്ട മെട്രോ സ്റ്റേഷനിൽ ഇറങ്ങുക.";
+var location_cab_txt = "തൃശ്ശൂർ, ആലപ്പുഴ ഭാഗത്തുനിന്ന് സ്വന്തം വാഹനത്തിലോ ടാക്സിയിലോ വരുന്നവർ വൈറ്റിലയിൽ നിന്നും തൃപ്പൂണിത്തുറ ഭാഗത്തേക്ക് തിരിഞ്ഞ് പേട്ട ജംഗ്ഷനിൽ എത്തി മരട് ഭാഗത്തേക്ക് തിരിയുക. അതുമല്ലെങ്കിൽ ഇവിടെ കൊടുത്തിരിക്കുന്ന ലൊക്കേഷൻ, ഗൂഗിൾ മാപ്പിൽ നോക്കി വരിക.";
+
+
+document.getElementById("title-txt").innerHTML = title_txt;
+
+document.getElementById("email-txt").innerHTML = email_txt;
+document.getElementById("phone-txt").innerHTML = phone_txt;
+document.getElementById("email-ftr-txt").innerHTML = email_txt;
+document.getElementById("phone-ftr-txt").innerHTML = phone_txt;
+
+document.getElementById("location-bus-txt").innerHTML = location_bus_txt;
+document.getElementById("location-train-txt").innerHTML = location_train_txt;
+document.getElementById("location-metro-txt").innerHTML = location_metro_txt;
+document.getElementById("location-cab-txt").innerHTML = location_cab_txt;
